c7f3cf2848d9870be482577ea82ce2bb4b663cef
Nico Zessoules <nicozessoules@gmail.com>
2024-01-19 18:55:48 -0700

    add header border shrink animation

--- src/scripts/layout.ts
+++ src/scripts/layout.ts
@@ -4,39 +4,65 @@ import { getDrawerElement, getHeaderBorderElement } from "./elements";
 import { delay } from "./utils";
 
 import headerStyles from "../components/header/header.module.scss";
+import navStyles from "../components/nav/nav.module.scss";
 
 export function isDrawerOpen() {
-  const drawer = getDrawerElement();
-  return drawer?.style?.opacity === "1";
+  const drawer = getDrawerElement() as HTMLElement;
+  return drawer.classList.contains(navStyles.drawerOpen);
+}
+
+export function closeDrawer() {
+  const drawer = getDrawerElement() as HTMLElement;
+  drawer.classList.remove(navStyles.drawerOpen);
+}
+
+export function openDrawer() {
+  const drawer = getDrawerElement() as HTMLElement;
+  drawer.classList.add(navStyles.drawerOpen);
 }
 
 export function toggleDrawer() {
-  const drawer = getDrawerElement();
-
-  if (drawer?.style?.transform === "translateX(-100%)") {
-    drawer.style.transform = "translateX(0)";
-    drawer.style.opacity = "1";
-  } else if (drawer?.style?.transform) {
-    drawer.style.transform = "translateX(-100%)";
-    drawer.style.opacity = "0";
+  if (isDrawerOpen()) {
+    closeDrawer();
+    return;
   }
+
+  openDrawer();
+}
+
+export function openHeaderBorder() {
+  if (window.location.pathname !== "/") return;
+
+  const headerBorder = getHeaderBorderElement() as HTMLElement;
+
+  headerBorder.classList.remove(headerStyles.headerBorderClose);
+  headerBorder.classList.add(headerStyles.headerBorderOpen);
+}
+
+export function closeHeaderBorder() {
+  const headerBorder = getHeaderBorderElement() as HTMLElement;
+
+  headerBorder.classList.remove(headerStyles.headerBorderOpen);
+  headerBorder.classList.add(headerStyles.headerBorderClose);
+}
+
+export function animateHeaderBorder(direction: "open" | "close") {
+  const { pathname } = window.location;
+
+  if (direction === "open") {
+    if (pathname !== "/") return;
+
+    openHeaderBorder();
+    return;
+  }
+
+  closeHeaderBorder();
 }
 
 export async function navigateHome() {
-  if (isDrawerOpen()) {
-    toggleDrawer();
-    await delay(500); // TODO set this time somewhere else
-  }
+  closeHeaderBorder();
+  if (isDrawerOpen()) toggleDrawer();
 
+  await delay(500); // TODO set this time somewhere else
   navigate("/");
-}
-
-export function animateHeaderBorder() {
-  const { pathname } = window.location;
-
-  if (pathname === "/") {
-    const headerBorder = getHeaderBorderElement();
-    if (headerBorder)
-      headerBorder.classList.add(headerStyles.animateHeaderBorder);
-  }
 }
